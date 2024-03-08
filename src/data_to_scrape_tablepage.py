075bd59a3b34d5455696ba4455e4b6b4b72eef40
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-08 12:30:27 +0100

    [Refactor] Optimized table_scrape_wide() function code

--- src/data_to_scrape_tablepage.py
+++ src/data_to_scrape_tablepage.py
@@ -3,7 +3,6 @@ import logging
 
 
 def table_scrape_last_games(html): # TODO  dokument + list helyett map or dict / not important /
-
     soup = BeautifulSoup(html, 'html.parser')
     
     table = soup.select_one("table.detailed-table")
@@ -21,35 +20,23 @@ def table_scrape_last_games(html): # TODO  dokument + list helyett map or dict /
     headers_table_wide = ["Team", "last 5"]
     return headers_table_wide, table_data
     
-    
 
 def table_scrape_wide(html): # csinálni + dokument
-
     soup = BeautifulSoup(html, 'html.parser')
     
     table = soup.select_one("table.detailed-table")
     table_rows = table.tbody.find_all('tr')
-    #headers = table.thead.find_all('th')
 
-    # TODO refactor to list comp to make it faster
     table_data = []
     for tr in table_rows:
-        td = tr.find_all('td')     
-        row = []
-        index = 0
-        for item in td:
-            if item.text.strip():
-                if index == 2:
-                    row.append(item.a.get('title').strip())
-                else:
-                    row.append(item.text.strip())
-            index += 1   
-        if row:
-            table_data.append(row)  
+        td = tr.find_all('td')  
+        row = [col.text.strip() for col in td if col.text.strip()]
+        row[1] = td[2].a.get('title').strip() # TODO merge with the list comp above
+        table_data.append(row)      
     logging.debug(table_data)   
     
     
-    headers_table_wide = ["index", "team", "MP-T", "W-T", "D-T", "L-T", "GF-T", "GA-T", "MP-H", "W-H", "D-H", "L-H", "GF-H", "GA-H", "MP-A", "W-A", "D-A", "L-A", "GF-A", "GA-A", "GD", "P"]
+    headers_table_wide = ["rank", "team", "MP-T", "W-T", "D-T", "L-T", "GF-T", "GA-T", "MP-H", "W-H", "D-H", "L-H", "GF-H", "GA-H", "MP-A", "W-A", "D-A", "L-A", "GF-A", "GA-A", "GD", "P"]
     return headers_table_wide, table_data
 
 def table_scrape_topscorers(html): # TODO dokument + reduce reused code / not important / 
@@ -60,7 +47,6 @@ def table_scrape_topscorers(html): # TODO dokument + reduce reused code / not im
     
     table = soup.select_one("table.playerstats")
     table_rows = table.tbody.find_all('tr')
-    #headers = table.thead.find_all('th')
 
     table_data = []
     for tr in table_rows:
@@ -76,12 +62,12 @@ def table_scrape_topscorers(html): # TODO dokument + reduce reused code / not im
 
 
 def table_scrape_over_under(html): # TODO  dokument + list helyett map or dict / not important /
-
     soup = BeautifulSoup(html, 'html.parser')
     #page_competition_1_block_competition_tables_11_block_competition_overunder_table_1_table
     table = soup.select_one("table.overundertable")
     table_rows = table.tbody.find_all('tr')
 
+    # TODO !!!!!!!!!!!!!!!!!!! list comp
     table_data = []
     for tr in table_rows:
         td = tr.find_all('td')     
@@ -102,7 +88,6 @@ def table_scrape_over_under(html): # TODO  dokument + list helyett map or dict /
     headers_table_over_under = ["index", "team", "mp", "0", "1", "2", "3", "4", "5", "6", "7", ">7", "avg"]
     return headers_table_over_under, table_data
     
-
 
 if __name__ == "__main__":
     pass
