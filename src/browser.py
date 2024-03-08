e5c6af8ebd008a147b7a3df8b987f43c67e10838
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-08 12:56:21 +0100

    [Refactor] previously named playwright_main.py | added comments

--- src/browser.py
+++ src/browser.py
@@ -7,6 +7,11 @@ import html_to_data_conversion
 
 
 # TODO mindent kiszervezni majd module/pkg  / not important /
+# TODO team-stat / másik link - vagy kattintás után csapat
+# TODO team-history / másik link - vagy kattintás után csapat
+# TODO
+
+
 def time_wait(wait_time_seconds: float, msg="no msg"):
   """Calls time.sleep(wait_time_seconds), then prints the debug log if logging is set to debug
   
@@ -15,6 +20,7 @@ def time_wait(wait_time_seconds: float, msg="no msg"):
       msg (str): message for the logger
 
   """
+  
   time.sleep(wait_time_seconds)
   logging.debug(f"waited: {wait_time_seconds} seconds [{msg}]")
 
@@ -48,42 +54,18 @@ def main(debug_slow_down=0, league_size_for_debug=1): # TODO külön class brows
     )
     page = browser.new_page()
 
-    for test_index in range(0,len(constans.LINKS_2023_2024)-(10-league_size_for_debug)):
-        league_team=test_index #TODO league or team name for output csv name:   
+
+    for index in range(0,(len(constans.LINKS_2023_2024)-11+league_size_for_debug)):
+        league_team=index #TODO league or team name for output csv name:   
         
         page.route("**/*", rb.intercept_route)
-        page.goto(constans.LINKS_2023_2024[test_index]) # TODO automatize scraping for all links
+        page.goto(constans.LINKS_2023_2024[index])
         #page.wait_for_timeout(1000)
       
         time_wait(0.5, msg="after page open")
-        handle_privacy_popup(page)
-        
+        handle_privacy_popup(page)    
         time_wait(0.5)
-        
-        """
-        
-        # TODO 1st last five table | 2nd wide click és wide table | 3rd topscorers table !!!!!!!!
-        match table_type:
-          case "last":
-            pass
-          case "wide":
-            page.get_by_role("listitem").filter(has_text="Wide").click()
-            time_wait(0.5) 
-          case "ou":
-            time_wait(0.5) #TODO DEL OR FIX IT
-            page.get_by_role("listitem").filter(has_text="Over/under").click()
-            time_wait(0.5) #TODO DEL OR FIX IT
-          case "top":
-            pass  
-          case "team-stat": # más link
-            pass
-          case "team-history": # más link
-            pass
-          case _:
-            pass #default --> last_5 and topscorer
-        
-        """
-              
+
         html_to_data_conversion.html_to_dataframe(html=page.content(), league_team=league_team, table_type="last")
         html_to_data_conversion.html_to_dataframe(html=page.content(), league_team=league_team, table_type="top")
         time_wait(2) 
@@ -94,11 +76,8 @@ def main(debug_slow_down=0, league_size_for_debug=1): # TODO külön class brows
         page.get_by_role("listitem").filter(has_text="Over/under").click()
         time_wait(2) 
         html_to_data_conversion.html_to_dataframe(html=page.content(), league_team=league_team, table_type="ou")
-
-    # TODO team-history
-    
+        
     logging.debug(f"DONE")
-
     #time_wait(1000000000000000)
     browser.close()
 
