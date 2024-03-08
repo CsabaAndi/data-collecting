d12dc4810d852f94a908200c3da1041f8bbbe618
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-03-08 12:31:39 +0100

    [Refactor] Deleted if/else as it is no longer needed

--- src/html_to_data_conversion.py
+++ src/html_to_data_conversion.py
@@ -6,13 +6,11 @@ import pandas as pd
 
 # TODO többi leszedett táblázatot is átalakitani majd csv or json !!!
 def html_to_dataframe(html, league_team="error", table_type="wide",date="3000"):
-  wide=False
   match table_type:
     case "last": #TODO gut
       headers, rows = data_to_scrape_tablepage.table_scrape_last_games(html) 
-    case "wide": #TODO gut
-      headers, rows = data_to_scrape_tablepage.table_scrape_wide(html) 
-      wide=True
+    case "wide": #TODO gut 
+      headers, rows = data_to_scrape_tablepage.table_scrape_wide(html)
     case "ou": #TODO gut
       headers, rows = data_to_scrape_tablepage.table_scrape_over_under(html) 
     case "top": #TODO gut
@@ -23,18 +21,14 @@ def html_to_dataframe(html, league_team="error", table_type="wide",date="3000"):
       #headers, rows = data_to_scrape_team.table_scrape_match_history_per_page(html)
       pass
     case _:
-      pass #default --> last_5 and topscorer
+      pass # TODO | default --> last_5 and topscorer
 
-  #headers = []
   df_table = pd.DataFrame(rows, columns=headers)
-  df_table.to_csv(f"../exported_data/{league_team}-{table_type}-{date}.csv", sep='\t', encoding='utf-8', index=False)
+  df_table.to_csv(f"../exported_data/{league_team}-{table_type}-{date}.csv", encoding='utf-8', index=False)
   
-  if wide:
-    df = df_table.set_index('index')
-  else:
-    df = df_table
+  df = df_table
     
-  # Converts the dataframe into str object with formatting
+  # TODO - delete | [test] Converts the dataframe into str object with formatting
   print(df.to_markdown())
   
   return 0
