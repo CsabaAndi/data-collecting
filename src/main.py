6494b6ac27f6a088ac7d8b2edacb5f22281e3cd6
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-05-01 19:20:13 +0200

    Removed arguments from main()

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from debug_pkg.logs import log as debuglog
 import browser
 
+def main(lgs=1):
+    browser.main(lgs)
 
-def main(debug_slow_down=0, lgs=1):
-    browser.main(debug_slow_down, lgs)
- 
     
 if __name__ == "__main__":
   with debuglog.timed():
-    main(0, 11)
+    main(11)
