31265357b33545a67269860716ea14da41e70fcc
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-05-01 19:11:53 +0200

    Added Dev constans to if statement for changing logging level

--- src/debug_pkg/logs/log.py
+++ src/debug_pkg/logs/log.py
@@ -1,6 +1,8 @@
 import logging 
 from contextlib import contextmanager
 import time
+import constans_pkg.constans as constans
+
 # megkéne oldani hogy jól adja át a z infot a futo processben 
 
 
@@ -15,7 +17,9 @@ def timed():
 
 logger = logging.getLogger(__name__)
 FORMAT = "[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s] %(levelname)s: %(message)s" 
-logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+if constans.DEV_MODE:
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 
 def log(message="no message added"):
